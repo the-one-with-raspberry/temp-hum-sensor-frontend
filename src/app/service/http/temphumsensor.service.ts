b4b821e9a865bc992d5a610afd7078ef653cbff4
berriz44 <72183318+the-one-with-raspberry@users.noreply.github.com>
2023-08-21 17:08:48 +0300

    updated accordingly to accomodate the raspberry pi

--- src/app/service/http/temphumsensor.service.ts
+++ src/app/service/http/temphumsensor.service.ts
@@ -8,11 +8,11 @@ export class TemphumsensorService {
   constructor(private hc: HttpClient) { }
 
   getInfo() {
-    return this.hc.get<any>('http://192.168.68.70:5000/api/v1/current/getInfo');
+    return this.hc.get<any>('http://localhost:5000/api/v1/current/getInfo');
   }
   getHistInfo(time: string) {
     if (time != undefined) {
-      return this.hc.get<any>(`http://192.168.68.70:5000/api/v1/hist/getInfo?time=${time}`);
+      return this.hc.get<any>(`http://localhost:5000/api/v1/hist/getInfo?time=${time}`);
     } else {
       throw new Error('Invalid time parameter.');
     }
