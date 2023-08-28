813b414614901a87506331c26307f079983e8ca2
the-one-with-raspberry <victorstefan.costin@gmail.com>
2023-08-28 12:11:37 +0300

    add dev mode

--- src/app/service/http/temphumsensor.service.ts
+++ src/app/service/http/temphumsensor.service.ts
@@ -1,18 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { platform } from 'process';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TemphumsensorService {
-  constructor(private hc: HttpClient) { }
+  constructor(private hc: HttpClient, private router: Router) { }
+
+  apiVer: string = '1';
+
+  baseUrl: string = platform === 'linux' ? `http://localhost:5000/api/v${this.apiVer}` : `http://192.168.68.70:5000/api/v${this.apiVer}`;
 
   getInfo() {
-    return this.hc.get<any>('http://192.168.68.70:5000/api/v1/current/getInfo');
+    return this.hc.get<any>(`${this.baseUrl}/current/getInfo`);
   }
   getHistInfo(time: string) {
     if (time != undefined) {
-      return this.hc.get<any>(`http://192.168.68.70:5000/api/v1/hist/getInfo?time=${time}`);
+      return this.hc.get<any>(`${this.baseUrl}/hist/getInfo?time=${time}`);
     } else {
       throw new Error('Invalid time parameter.');
     }
